1361513b99de973cc15fa364da6d20cb4f856165
testudo <enes0bulut0@gmail.com>
2015-06-17 22:26:22 +0300

    dfefefef

--- ProjectEuler/Problem_2.cs
+++ ProjectEuler/Problem_2.cs
@@ -8,5 +8,37 @@ namespace ProjectEuler
 {
     class Problem_2
     {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("2. Problem's result: " + Problem_2.findSum(4000000));
+
+        }
+
+        public static int findSum(int limit)
+        {
+
+            int i = 1, j = 2, sum = 0, mem;
+
+            for (; ; )
+            {
+
+                //Console.WriteLine (i);
+
+                if (j >= limit)
+                    break;
+
+                if (j % 2 == 0)
+                    sum += j;
+
+                mem = j;
+                j += i;
+                i = mem;
+
+            }
+
+            return sum;
+
+        }
     }
 }
