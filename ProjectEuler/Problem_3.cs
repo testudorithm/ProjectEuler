f45da1e7b3d511dd90a1a329d2d9469af238dd2f
testudo <enes0bulut0@gmail.com>
2015-06-20 00:46:24 +0300

    problem 3

---
+++ ProjectEuler/Problem_3.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+    class Problem_3
+    {
+        public static void Main(string[] args)
+        {
+            long number = 600851475143L;
+
+            Console.WriteLine("Result: " + countResArr(number));
+
+            Console.ReadLine();
+
+        }
+
+        public static string countResArr(long x)
+        {
+
+            string result = "";
+
+            for (int i = 2; i <= x; i++)
+            {
+
+                if (x == 1)
+                {
+
+                    break;
+
+                }
+
+                if (x % i == 0 && isPrimeNum(i))
+                {
+
+                    result += i + " ";
+                    x /= i;
+
+                }
+
+            }
+
+            return result;
+
+        }
+
+        public static bool isPrimeNum(int x)
+        {
+
+            for (int i = 2; i <= x; i++)
+            {
+
+                if (i != x && x % i == 0)
+                {
+
+                    return false;
+
+                }
+
+            }
+
+            if (x < 2)
+                return false;
+
+            return true;
+
+        }
+    }
+}
