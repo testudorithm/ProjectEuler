49525ec5e7e89baf9aa80c815516e0cf23685e5e
testudo <enes0bulut0@gmail.com>
2015-06-27 18:13:08 +0300

    problem 9, 10, 11

---
+++ ProjectEuler/Problem_10.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+    class Problem_10
+    {
+        public static long findSumofPrimes(int limit)
+        {
+
+            long result = 0;
+
+            for (long i = 0; i < limit; i++)
+            {
+
+                if (isPrimeNum((int)i))
+                    result += i;
+
+            }
+
+            return result;
+
+        }
+
+        public static bool isPrimeNum(int x)
+        {
+
+            for (int i = 2; i <= x; i++)
+            {
+
+                if (i != x && x % i == 0)
+                {
+
+                    return false;
+
+                }
+
+            }
+
+            if (x < 2)
+                return false;
+
+            return true;
+
+        }
+    }
+}
---
+++ ProjectEuler/Problem_9.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+    class Problem_9
+    {
+        private static long limit = 1000;
+
+        public static int calculatePythagoreanTriplet()
+        {
+
+            for (int i = 1; i < limit; i++)
+            {
+
+                for (int j = i + 1; j < limit; j++)
+                {
+
+                    int tempThirdNum = getPythThirdNum(i, j);
+
+                    if (tempThirdNum != -1 && (tempThirdNum + i + j) == 1000 && (j < tempThirdNum))
+                    {
+
+                        return i * j * tempThirdNum;
+
+                    }
+
+                }
+
+            }
+
+            return -1;
+
+        }
+
+
+        public static int getPythThirdNum(int i, int j)
+        {
+
+
+            int pythSum = (i * i) + (j * j);
+            double pythSqrt = Math.Sqrt(pythSum);
+
+            if (((int)pythSqrt * (int)pythSqrt) == (pythSum))
+            {
+
+                return (int)pythSqrt;
+
+            }
+
+            return -1;
+
+
+        }
+    }
+}
