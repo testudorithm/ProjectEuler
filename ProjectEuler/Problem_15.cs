613e4734e2baf6f253f7d7f5ba79a3de53ddefb9
testudo <enes0bulut0@gmail.com>
2015-07-04 17:59:52 +0300

    problem 15,16

---
+++ ProjectEuler/Problem_15.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace ProjectEuler
+{
+    class Problem_15
+    {
+        private static int matrixEdgeNum = 20;
+
+        public static BigInteger findResult()
+        {
+
+            return takeFac(matrixEdgeNum * 2) / (takeFac(matrixEdgeNum) * takeFac(matrixEdgeNum));
+
+        }
+
+        public static BigInteger takeFac(int x)
+        {
+
+            BigInteger temp = 1;
+
+            for (int i = 1; i <= x; i++)
+            {
+
+                temp *= i;
+
+            }
+
+            return temp;
+
+        }
+    }
+}
---
+++ ProjectEuler/Problem_16.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+    class Problem_16
+    {
+
+        public static long findResult()
+        {
+
+            return sumOfDigits(power(1000, 2).ToString());
+
+        }
+
+        public static long sumOfDigits(string x)
+        {
+
+            long tempSum = 0;
+
+            foreach (char y in x)
+            {
+
+                tempSum += Convert.ToInt32(y.ToString());
+
+            }
+
+
+            return tempSum;
+
+        }
+
+
+        public static BigInteger power(int x, int y)
+        {
+
+            BigInteger temp = 1;
+
+            for (int i = 1; i <= x; i++)
+            {
+
+                temp *= y;
+
+            }
+
+            return temp;
+
+        }
+    }
+}
